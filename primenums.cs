159a6e9105519063ac675a00030cd25582779938
kelthor-the-creator <57299270+kelthor-the-creator@users.noreply.github.com>
2020-10-26 13:15:03 +0000

    Add files via upload

---
+++ primenums.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Lab_0
+{
+    public class primenums
+    {
+
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("prime numbers: ");
+            bool isprime = true;
+
+            for (int i= 2; i<= 15; i++)
+            {
+                for(int j = 2; j<= 15; j++)
+                {
+                    if( i != j && i%j== 0)
+                    {
+                        isprime = false;
+                            break;
+                    }
+                }
+                if (isprime)
+                {
+                    Console.Write("\t " + i);
+                }
+                isprime = true;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
