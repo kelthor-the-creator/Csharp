50bb8460dc96aeaf9d7009ce68b4a7f5ad3c2c03
kelthor-the-creator <57299270+kelthor-the-creator@users.noreply.github.com>
2020-11-01 16:16:31 +0000

    sieve of erantothenes code

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SieveErantothenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 50;
+            sieve prime = new sieve();
+            sieve.sieveOferatosthenes(n);
+           
+        }
+    }
+}
